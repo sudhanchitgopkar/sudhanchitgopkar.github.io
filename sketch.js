e5199604888a581562047ddc0aa5b3177958ab93
Sudhan Chitgopkar <sudhan@osx.local>
2022-06-09 18:57:40 -0700

    added: new site components

--- sketch.js
+++ sketch.js
@@ -1,78 +1,70 @@
-var canvas;
-var numPoints;
-var currPoint;
-var modVal;
-var modCount;
-var flag;
-
-
-
 function windowResized() {
-    var canvasDiv = document.getElementById('sketch');
-    var width = canvasDiv.offsetWidth;
-    var height = canvasDiv.offsetHeight;
-    resizeCanvas(width, height);
-    location.reload();
+    resizeCanvas(windowWidth, windowHeight);
 }
+
+class Mover{
+    constructor() {
+      this.position = createVector(random(width), random());
+      this.velocity = createVector();
+      this.acceleration = createVector();
+      this.topspeed = 7;
+      this.midpoint = createVector(width/2,height/2);
+    }
+  
+    update(target) {
+        this.acceleration = p5.Vector.sub(target,this.position);
+        this.acceleration.setMag(random(0.15,0.21));
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.topspeed);
+        this.position.add(this.velocity);
+        return this.position;
+    }
+
+    display() {
+      ellipse(this.position.x, this.position.y, 1, 1);
+    }
+  }
+
+ m = new Array(200);  
+ var target;
+ var perX;
+ var perY;
 
 function setup() {
-    var canvasDiv = document.getElementById('sketch');
-    var width = canvasDiv.offsetWidth;
-    var height = canvasDiv.offsetHeight;
-    var sketchCanvas = createCanvas(width,height);
-    console.log(sketchCanvas);
-    sketchCanvas.parent("sketch");
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index', '-1');
+  background(0);
+  colorMode(HSB);
 
-    frameRate(30);
-    background(0);
-    
-    stroke(255);
-    noFill();
-    
-    numPoints=300;
-    currPoint=1;
-    modVal=2;
-    modCounter=1;
-    flag = false;
-  
-    circle(width/2,height/2-25,width);
-}
+  perX = 0.5;
+  perY = 0.1;
+  for (let i = 0; i < m.length; i++) {
+    m[i] = new Mover();
+  } //for
+ } //seup
 
-function draw() {
-    if (modCounter > 310)
-    flag = true;
-  else if(modCounter < 1)
-    flag = false
-  
-  if(!flag) {
-    strokeWeight(0.5);
-    //stroke(map(currPoint,0,numPoints,0,255),69,69);
-    stroke('rgba(255, 109, 109, 0.3)');
-    line((width/2-3)*cos((currPoint/numPoints) * 2*PI) + width/2,
-    (width/2-3)*sin((currPoint/numPoints) * 2*PI) + height/2-25,
-    (width/2-3)*cos((modVal*currPoint/numPoints%numPoints) * 2*PI) + width/2,
-    (width/2-3)*sin((modVal*currPoint/numPoints%numPoints) * 2*PI) + height/2-25);
-    currPoint++;
-    modCounter++;
-  }
-  
-  if(flag) {
-    strokeWeight(1.2);
-    stroke(0);
-    line((width/2-3)*cos((currPoint/numPoints) * 2*PI) + width/2,
-    (width/2-3)*sin((currPoint/numPoints) * 2*PI) + height/2-25,
-    (width/2-3)*cos((modVal*currPoint/numPoints%numPoints) * 2*PI) + width/2,
-    (width/2-3)*sin((modVal*currPoint/numPoints%numPoints) * 2*PI) + height/2-25);
-    currPoint--;
-    modCounter--;
-    
-    if(modCounter == 1) {
-      modVal+=23;
-    } //if
-  } //if
-}
-  
-function mouseClicked() {
-    location.reload();
-  }
-  
+ function draw() {
+      background(0,0.05);
+      if (mouseX <= 0 || mouseY <= 0 || mouseX >= width || mouseY >= height) {
+        target = createVector(map(noise(perX),1,0,0,width),map(noise(perY),1,0,0,height));
+        perX+=random(0.005,0.007);
+        perY+=random(0.005,0.007);
+      } else {
+        target = createVector(mouseX,mouseY);
+      } //if
+    noFill(); stroke(255);
+    //circle(target.x,target.y,7);
+      for (let i = 0; i < m.length; i++) {
+        stroke(map(m[i].update(target).x,0,width,0,255),255,255);
+        m[i].display();
+      } //for  
+ }
+
+ function mouseClicked() {
+     background(0);
+     m = new Array(100);  
+     for (let i = 0; i < m.length; i++) {
+        m[i] = new Mover();
+      } //for
+ }
